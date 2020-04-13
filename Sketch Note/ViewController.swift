9f48f1bc88f00c67f91a5c6ed81a0f43086a1180
michael tamsil <michaeltamsil@gmail.com>
2020-04-13 20:53:00 +0700

    Initial Commit

---
+++ Sketch Note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sketch Note
+//
+//  Created by michael tamsil on 13/04/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
